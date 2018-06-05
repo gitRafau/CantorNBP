03ca6daefbb794e9ca9039190b1e51ed4091b62e
Rafal Fidurski <rafal.fidurski@gmail.com>
2018-06-05 18:01:38 +0200

    api, price of gold

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,11 +6,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-    //
-//    public function homeAction(Request $request)
-//    {
-//        return $this->render('cantor/home.html.twig');
-//    }
+class DefaultController extends Controller {
+
+    
+
 }
